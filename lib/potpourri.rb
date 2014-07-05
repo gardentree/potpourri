dc9691ea2f6570211830f816f176eaf7a56f9ff3
GardenTree <gardentreex@gmail.com>
2014-07-05 22:02:38 +0900

    Make stamp

--- lib/potpourri.rb
+++ lib/potpourri.rb
@@ -1,5 +1,22 @@
 require "potpourri/version"
 
 module Potpourri
-  # Your code goes here...
+  def self.stamp
+    time = Time.now.strftime("%Y%m%d%H%M")
+
+    begin
+      execute("git tag #{time}")
+      execute("git push origin #{time}")
+
+      puts "#{time} was created"
+    rescue => error
+      STDERR.puts "error [#{error}]"
+    end
+  end
+
+  private
+    def self.execute(command)
+      puts "execute [#{command}]"
+      raise command unless system(command)
+    end
 end
